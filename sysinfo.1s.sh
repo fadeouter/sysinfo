23a44f9e4ea35cd828a00eead8542d3dde6a8f8d
fadeouter <my.own.anonymous.emails.collector@gmail.com>
2017-04-24 20:39:42 +0300

    3

--- sysinfo.1s.sh
+++ sysinfo.1s.sh
@@ -7,16 +7,81 @@
 # before use: sudo apt install top sysstat
 
 
+bg_color="#c9dfe5"	# pie bg color
+fg_color="#8c5ea0"	# pie fill color
+hw="16px"		# adjust this variable to your screen DPI
+
+
+################################################################
+#
+#  CPU
+#
+################################################################
+
+CPU=$(echo $[100-$(vmstat 1 2|tail -1|awk '{print $15}')])
+CPUD=$(echo $CPU | awk '{print $1 * 0.1}' | awk '{ printf("%.0f\n", $1); }' | awk '{print $0"0"}')
+top=$(top -bn 1 | grep -v "top\|Tasks\|Cpu\|KiB" | awk '{ printf("%-4s %-s\n", $9 / 2, $12); }' | head -n 5 | tail -n+3 | awk 1 ORS="\\\\n")
+top_cpu=$(echo $top | sed 's/\\n/ /g' | awk '{ print $1 + $3 + $5}')
+
+if [ "$top_cpu" \> "$CPU" ]; then
+CPU=$(echo "$top_cpu")
+fi
+
+cpu_icon=$(echo "<svg xmlns='http://www.w3.org/2000/svg' width='120px' height='120px' viewBox='0 0 120px 120px'><g transform='translate(0,120) scale(1, -1)'><rect rx='7px' y='10px' x='25px' height='100px' width='50px' fill='$bg_color' /><rect rx='7px' y='10px' x='25px' height='$top_cpu' width='50px' fill='$fg_color' /></g></svg>" | base64 -w 0)
+
+echo "| image=$cpu_icon imageHeight=$hw"
+
+echo "---"
+
+echo "$top<span font='5'>--------------------------</span>\n<span color='#444444' font='10.5'><b>$CPU   total</b></span>| font=monospace size=10 image=$cpu_icon imageHeight=16px bash=gnome-system-monitor terminal=false"
+
+################################################################
+#
+#  Mem
+#
+################################################################
+
 raw_mem=$(free -m | grep Mem)
 raw_swap=$(free -m | grep Swap)
 mem_used=$(echo $raw_mem | awk '{print $2 - $7}')
+mem_D=$(echo $raw_mem | awk '{print (($2 - $7) / $2) * 10}' | awk '{ printf("%.0f\n", $1); }' | awk '{print $0"0"}')
 mem_full=$(echo $raw_mem | awk '{print $2}')
-mem=$(echo $raw_mem | awk '{print (($2 - $7)/$2) * 100.0}')
 swap_used=$(echo $raw_swap | awk '{print $3}')
 swap_full=$(echo $raw_swap | awk '{print $2}')
-swap=$(echo $raw_swap | awk '{print $3/$2 * 100.0}')
-CPU=$(echo $[100-$(vmstat 1 2|tail -1|awk '{print $15}')])
-top=$(top -bn 1 | grep -v "top\|Tasks\|Cpu\|KiB" | awk '{ printf("%-8s %-8s\n", $9, $12); }' | head -n 5 | tail -n+3 | awk 1 ORS="\\\\n" )
+swap_D=$(echo $raw_swap | awk '{print ($4 / $2) * 10}' | awk '{ printf("%.0f\n", $1); }' | awk '{print $0"0"}')
+
+
+pie_start="<svg width='$hw' height='$hw' viewBox='0 0 90.146759 90.144005'><g transform='translate(-59.928 -103.428)'><circle cx='105' cy='148.5' r='45.979' fill='$bg_color'/>"
+pie_00="</g></svg>"
+pie_10="<path d='M105 103.52a44.98 44.98 0 0 1 26.438 8.592L105 148.5z' fill='$fg_color' /></g></svg>"
+pie_20="<path d='M105 103.52a44.98 44.98 0 0 1 42.777 31.08L105 148.5z' fill='$fg_color' /></g></svg>"
+pie_30="<path d='M105 103.52a44.98 44.98 0 0 1 36.388 18.542 44.98 44.98 0 0 1 6.39 40.337L105 148.5z' fill='$fg_color' /></g></svg>"
+pie_40="<path d='M105 103.52a44.98 44.98 0 0 1 44.98 44.98A44.98 44.98 0 0 1 105 193.48V148.5z' fill='$fg_color' /></g></svg>"
+pie_50="<path d='M105 103.52a44.98 44.98 0 0 1 44.98 44.98A44.98 44.98 0 0 1 105 193.48V148.5z' fill='$fg_color' /></g></svg>"
+pie_60="<path d='M105 103.52a44.98 44.98 0 0 1 42.777 31.08 44.98 44.98 0 0 1-16.34 50.288 44.98 44.98 0 0 1-52.875 0L105 148.5z' fill='$fg_color' /></g></svg>"
+pie_70="<path d='M105 103.52a44.98 44.98 0 0 1 44.732 40.278 44.98 44.98 0 0 1-35.38 48.698 44.98 44.98 0 0 1-52.13-30.097L105 148.5z' fill='$fg_color' /></g></svg>"
+pie_80="<path d='M105 103.52a44.98 44.98 0 0 1 42.777 31.08 44.98 44.98 0 0 1-16.34 50.288 44.98 44.98 0 0 1-52.875 0 44.98 44.98 0 0 1-16.34-50.287L105 148.5z' fill='$fg_color' /></g></svg>"
+pie_90="<path d='M105 103.52a44.98 44.98 0 0 1 44.425 37.944 44.98 44.98 0 0 1-30.526 49.813 44.98 44.98 0 0 1-53.976-22.357 44.98 44.98 0 0 1 13.638-56.808L105 148.5z' fill='$fg_color' /></g></svg>"
+pie_100="<circle cx='105' cy='148.5' r='44.979' fill='$fg_color' /></g></svg>"
+
+pie_sw="pie_$swap_D"
+pie_sw=$(echo "${!pie_sw}")
+pie_sw=$(echo "$pie_start$pie_sw" | base64 -w 0)
+
+pie_mem="pie_$mem_D"
+pie_mem=$(echo "${!pie_mem}")
+pie_mem=$(echo "$pie_start$pie_mem" | base64 -w 0)
+
+echo "${mem_used%%.*} / ${mem_full%%.*} MiB | image=$pie_mem"
+echo "${swap_used%%.*} / ${swap_full%%.*} MiB | image=$pie_sw"
+
+echo "---"
+
+################################################################
+#
+#  Disk cap
+#
+################################################################
 
 OLDIFS=$IFS
 name=()
@@ -34,7 +99,7 @@ get_disk_stats() {
     IFS=$OLDIFS
     for ((i = 0; i < ${#dfdata[@]}; i++)); do
         line=(${dfdata[$i]})
-        if [[ "${line[0]}" == /dev/mapper* ]] || [[ "${line[0]}" == /dev/sd* ]]; then
+        if [[ "${line[0]}" == /dev/mapper* ]] || [[ "${line[0]}" == /dev/sdb* ]]; then
             name+=("${line[5]}")
             cap+=("${line[1]}")
             used+=("${line[2]}")
@@ -44,21 +109,15 @@ get_disk_stats() {
     done
 }
 
-echo "<b>$CPU%</b>   ${mem%%.*}/${swap%%.*} | size=10 iconName=utilities-system-monitor-symbolic"
-echo "---"
-echo "Mem:   ${mem_used%%.*} / ${mem_full%%.*} MiB"
-echo "Swap:   ${swap_used%%.*} / ${swap_full%%.*} MiB"
-echo "---"
-echo "$top | font=monospace size=10 trim=true"
-echo "---"
-
 get_disk_stats
+
+echo "---"
 
 for ((i = 0; i < ${#capacity[@]}; i++)); do
 	echo "${cap[$i]}   <span color='#555555' font='10'>${name[$i]}</span> | iconName=drive-harddisk-system length=20"
 	echo "${used[$i]} / <span color='green'>${free[$i]}</span> (${capacity[$i]} %)| refresh=false  iconName=image-filter-symbolic"
         echo "---"
 done
-
 echo "Check free space | iconName=baobab bash=baobab terminal=false"
 echo "Open System Monitor | iconName=utilities-system-monitor bash=gnome-system-monitor terminal=false"
+
